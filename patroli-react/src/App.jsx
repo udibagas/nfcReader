2385194fc6999823dd69c0d43d495d1c9f847b61
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-30 16:38:05 +0700

    add transition

--- patroli-react/src/App.jsx
+++ patroli-react/src/App.jsx
@@ -1,18 +1,47 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "./App.css";
+import { useRef } from "react";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
+  );
+};
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames="fade"
+        timeout={300}
+        nodeRef={nodeRef}
+      >
+        <div ref={nodeRef}>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
